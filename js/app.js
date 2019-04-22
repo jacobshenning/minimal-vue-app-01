fe2e79a91b8c67a332bbbc1b9d9c4b8ce0d09a30
Jacob Henning <jacobshenning@gmail.com>
2019-04-22 16:21:20 -0700

    Initial Vue App Rendering

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        page: {
+            title: 'Hello World',
+            subtitle: "A vue App",
+            p1: "Etiam porttitor elit ut enim euismod aliquam. Sed a leo lacus. Suspendisse a tortor in odio tincidunt molestie sed quis lectus. Sed molestie viverra turpis eget volutpat. Pellentesque et aliquet mauris. Fusce eu dolor eget mi hendrerit gravida. Donec a erat urna.",
+            p2: "Pellentesque in nisl aliquet, consequat felis non, fringilla risus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sit amet nisl sed felis suscipit elementum et vitae felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ut neque facilisis orci accumsan aliquet. In hac habitasse platea dictumst. Integer eget mauris vel diam rhoncus dignissim. Aliquam dictum pharetra quam eu accumsan. Vivamus mauris ipsum, mattis et erat in, porta maximus dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras molestie nisl augue, quis luctus justo pellentesque quis. Aliquam egestas metus sit amet massa fermentum consequat. Nunc sit amet porta magna. Praesent efficitur ex sem, id auctor eros condimentum sit amet. Etiam nec posuere sapien, sed fringilla dui. Ut condimentum pretium velit, ac molestie nulla.",
+            p3: "Maecenas venenatis ipsum id ligula volutpat, blandit dignissim mauris egestas. Etiam quis faucibus lacus. Vivamus elit tellus, egestas ac erat sed, lobortis accumsan lectus. Curabitur sodales ac turpis porta gravida. Fusce eget metus efficitur tortor consequat luctus quis sed ligula. Donec vitae nisi risus. Fusce id luctus massa, vel cursus ipsum. Maecenas placerat laoreet vehicula."
+        }
+    }
+})
